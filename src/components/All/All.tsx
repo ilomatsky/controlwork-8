658ec02df1c9fb2d5261adc69184be7a3b676f64
Ilya Lomatsky <ilomatsky@gmail.com>
2023-12-02 14:20:13 +0600

    Displaying all quotes on the page

--- src/components/All/All.tsx
+++ src/components/All/All.tsx
@@ -1,7 +1,40 @@
+import {useState, useEffect} from 'react';
+import axiosAPI from '../../axiosAPI';
+
+interface Post {
+  id: string;
+  author: string;
+  text: string;
+}
+
 const All = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axiosAPI.get<{ [key: string]: Post }>('/quotes.json');
+        const fetchedPosts = Object.values(response.data);
+        setPosts(fetchedPosts);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div>
-      All
+      <h2>All Posts</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h3>{post.author}</h3>
+            <p>{post.text}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
